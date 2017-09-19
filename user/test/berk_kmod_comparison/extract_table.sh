183e2083e8d57cb93dc9709f181af211fa2ed76e
Abhishek Ghogare <avg598@gmail.com>
2017-09-19 00:01:09 -0400

    updated extract table script

--- user/test/berk_kmod_comparison/extract_table.sh
+++ user/test/berk_kmod_comparison/extract_table.sh
@@ -1,13 +1,55 @@
 #!/bin/bash
-
-for f in `ls *redis*run*log`; 
+declare -A pairs
+for f in `ls *redis*instance-1*run*log`; 
 do
 	#echo -n $f | tr '_.' ' ' | awk '{print $2 "\t" $3 "\t" $7 "\t" $9 "\t" $11 "\t" $12;}' | tr "\n" '\t';
 	echo -n $f | tr '-' ' ';
+	echo -n " throughput " ; 
 	cat $f | grep "Throughput(ops/sec)" | awk -v FS='[ ,]+' '{print " ", $3;}' | tr "\n" '\t';
-	pagefaults=`cat $f | grep "pagefault" | awk -v FS='[ ,]+' '{print " ", $3;}'`
-	echo "$pagefaults";
-	
+
+	cat $f | grep '^inst: 0' | while read -r line;
+	do
+		instid=$(echo $line | sed 's/[ \t\r\n:]\+/ /g' | cut -d' ' -f2);
+		perm=$(echo $line | sed 's/[ \t\r\n]\+/ /g' | cut -d' ' -f3);
+		pfcount=$(echo $line | sed 's/[ \t\r\n]\+/ /g' | cut -d' ' -f4);
+
+		pairs["${instid}-${perm}"]=${pfcount};
+		echo -n " RUN:${instid}-${perm} ${pfcount}";
+	done;
+	fload=$(echo $f | sed 's/-run/-load/g')
+	cat $fload | grep '^inst: 0' | while read -r line;
+	do
+		instid=$(echo $line | sed 's/[ \t\r\n:]\+/ /g' | cut -d' ' -f2);
+		perm=$(echo $line | sed 's/[ \t\r\n]\+/ /g' | cut -d' ' -f3);
+		pfcount=$(echo $line | sed 's/[ \t\r\n]\+/ /g' | cut -d' ' -f4);
+
+		echo -n " LOAD:${instid}-${perm} ${pfcount}";
+		#echo -n " ${instid}-${perm} ${pairs[${instid}-${perm}]}";
+	done;
+
+	if [ $(echo $f | grep "kmod_process_in_module-separate" | wc -l) -gt 0 ];
+	then
+		cat $f | grep '^inst: 1' | while read -r line;
+		do
+			instid=$(echo $line | sed 's/[ \t\r\n:]\+/ /g' | cut -d' ' -f2);
+			perm=$(echo $line | sed 's/[ \t\r\n]\+/ /g' | cut -d' ' -f3);
+			pfcount=$(echo $line | sed 's/[ \t\r\n]\+/ /g' | cut -d' ' -f4);
+
+			pairs["${instid}-${perm}"]=${pfcount};
+			echo -n " RUN:${instid}-${perm} ${pfcount}";
+		done;
+		fload=$(echo $f | sed 's/-run/-load/g')
+		cat $fload | grep '^inst: 1' | while read -r line;
+		do
+			instid=$(echo $line | sed 's/[ \t\r\n:]\+/ /g' | cut -d' ' -f2);
+			perm=$(echo $line | sed 's/[ \t\r\n]\+/ /g' | cut -d' ' -f3);
+			pfcount=$(echo $line | sed 's/[ \t\r\n]\+/ /g' | cut -d' ' -f4);
+
+			echo -n " LOAD:${instid}-${perm} ${pfcount}";
+			#echo -n " ${instid}-${perm} ${pairs[${instid}-${perm}]}";
+		done;
+	fi
+	echo ""
 done;
 
 # to take average of all tests
